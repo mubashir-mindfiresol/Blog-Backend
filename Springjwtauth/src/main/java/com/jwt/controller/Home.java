f16106b240c345d3ceda63bf8743ebbab29e83c5
varsha2000 <varshac.1908@gmail.com>
2021-03-16 01:00:01 +0530

    jwt authentication

---
+++ Springjwtauth/src/main/java/com/jwt/controller/Home.java
@@ -0,0 +1,15 @@
+package com.jwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+	@RequestMapping("/welcome")
+	public String Welcome() {
+		String text="this is a private mesaage";
+		text +="this page is not allowed to unauthenticated user";
+		return text;
+	}
+
+}
